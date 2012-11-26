f34197714d02eb19822a24110c1eb4b270740c6c
Kore Nordmann <mail@kore-nordmann.de>
2012-11-26 10:35:53 +0100

    Added: Smaller ewxample used for profiling

---
+++ docs/profile_example.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace eZ\Publish\Profiler;
+
+// Makes the following variables available:
+// - $repository (Access to Public API)
+// - $persistenceHandler (Access to SPI (Persistence Handler))
+// - $dbHandler (Direct access to the currently used database handler)
+require __DIR__ . '/bootstrap.php';
+
+$folderType = new ContentType(
+    'folder',
+    array(
+        'title' => new Field\TextLine(),
+    )
+);
+
+$articleType = new ContentType(
+    'article',
+    array(
+        'title' => new Field\TextLine(),
+        'body' => new Field\XmlText( new DataProvider\XmlText() ),
+        'author' => new Field\Author( new DataProvider\User( 'editor' ) ),
+        // …
+    )
+);
+
+$commentType = new ContentType(
+    'comment',
+    array(
+        'text' => new Field\TextBlock(),
+        'author' => new Field\Author( new DataProvider\Aggregate( array(
+            new DataProvider\AnonymousUser(),
+            new DataProvider\User( 'user' )
+        ) ) ),
+        // …
+    )
+);
+
+$createTask = new Task(
+    new Actor\Create(
+        1, $folderType,
+        new Actor\Create(
+            12, $folderType,
+            new Actor\Create(
+                50, $articleType,
+                new Actor\Create(
+                    5, $commentType
+                ),
+                $articles = new Storage\LimitedRandomized()
+            )
+        )
+    )
+);
+
+$viewTask = new Task(
+    new Actor\SubtreeView(
+        $articles
+    )
+);
+
+/* Not implemented yet:
+$searchTask = new Task(
+    new Actor\Search( array(
+        'foo',
+        'bar'
+    ) )
+); // */
+
+$logger = new Logger\Statistics();
+$executor = new Executor\SPI(
+    $persistenceHandler,
+    $repository->getFieldTypeService(),
+    array(
+        new Constraint\Ratio( $createTask, 1 ),
+    ),
+    $logger,
+    new Aborter\ContentObjectAttributeCount(
+        $dbHandler,
+        200
+    )
+);
+$executor->run();
+
+$articles->reset();
+$executor = new Executor\PAPI(
+    $repository,
+    array(
+        new Constraint\Ratio( $createTask, 1/5 ),
+        new Constraint\Ratio( $viewTask, 1 ),
+    ),
+    $logger,
+    new Aborter\ContentObjectAttributeCount(
+        $dbHandler,
+        250
+    )
+);
+$executor->run();
+
+$logger->showSummary();
+
