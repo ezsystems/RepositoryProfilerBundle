ebee53916fd262f1eabe0e3f3e3b311519802c61
Kore Nordmann <mail@kore-nordmann.de>
2015-05-06 09:29:56 +0200

    Implemented count aborter

---
+++ src/php/eZ/Publish/Profiler/Aborter/Count.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace eZ\Publish\Profiler\Aborter;
+
+use eZ\Publish\Profiler\Aborter;
+use eZ\Publish\Core\Persistence\Legacy\EzcDbHandler;
+
+class Count extends Aborter
+{
+    protected $count = 0;
+
+    protected $iterationCount;
+
+    public function __construct( $iterationCount )
+    {
+        $this->iterationCount = $iterationCount;
+    }
+
+    public function shouldAbort()
+    {
+        return (++$this->count >= $this->iterationCount);
+    }
+}
+
