b801e4d708d3403546540351d4daec26ffbd2f49
Kore Nordmann <mail@kore-nordmann.de>
2015-05-08 10:54:26 +0200

    Print better statistics

--- src/php/eZ/Publish/Profiler/Logger/Statistics.php
+++ src/php/eZ/Publish/Profiler/Logger/Statistics.php
@@ -61,36 +61,59 @@ class Statistics extends Logger
 
             foreach ( $this->actor[$executor] as $type => $stats )
             {
-                printf(
-                    " * %s: %d (%.2f per second)" . PHP_EOL,
-                    $type,
-                    count( $stats ),
-                    count( $stats ) / $runtime
+                $statistics = $this->getStatistics(
+                    array_map(
+                        function ( $stats )
+                        {
+                            return ( $stats['end'] - $stats['start'] ) * 1000;
+                        },
+                        $stats
+                    ),
+                    $runtime
                 );
 
-                $runtimes = array_map(
-                    function ( $stats )
-                    {
-                        return ( $stats['end'] - $stats['start'] ) * 1000;
-                    },
-                    $stats
-                );
-
-                printf(
-                    "   - Minimum: %.2f ms" . PHP_EOL,
-                    min( $runtimes )
-                );
-                printf(
-                    "   - Average: %.2f ms" . PHP_EOL,
-                    array_sum( $runtimes ) / count( $runtimes )
-                );
-                printf(
-                    "   - Maximum: %.2f ms" . PHP_EOL,
-                    max( $runtimes )
-                );
+                printf( " * %s: %d (%.2f per second)" . PHP_EOL, $type, $statistics['count'], $statistics['per_second'] );
+                printf( "   - Minimum.......: %.2f ms" . PHP_EOL, $statistics['minimum'] );
+                printf( "   - 90%% Percentile: %.2f ms" . PHP_EOL, $statistics['90'] );
+                printf( "   - Median........: %.2f ms" . PHP_EOL, $statistics['median'] );
+                printf( "   - Average.......: %.2f ms" . PHP_EOL, $statistics['average'] );
+                printf( "   - Maximum.......: %.2f ms" . PHP_EOL, $statistics['maximum'] );
+                printf( "   - Std. Deviation: %.2f ms" . PHP_EOL, $statistics['std_deviation'] );
             }
-
         }
+    }
+
+    /**
+     * Get statistics
+     *
+     * @param array $runtimes
+     * @return array
+     */
+    protected function getStatistics( array $runtimes, $runtime )
+    {
+        sort($runtimes);
+
+        $count = count($runtimes);
+        $sum = array_sum($runtimes);
+        $average = $sum / $count;
+
+        $variance = 0;
+        foreach ($runtimes as $duration) {
+            $variance += pow($duration - $average, 2) / $count;
+        }
+
+        return array(
+            'count' => $count,
+            'sum' => $sum,
+            'per_second' => $count / $runtime,
+            'minimum' => reset($runtimes),
+            'maximum' => end($runtimes),
+            'average' => $average,
+            '90' => array_sum(array_slice($runtimes, 0, $c90 = floor($count * .9))) / $c90,
+            'median' => $runtimes[floor($count / 2)],
+            'variance' => $variance,
+            'std_deviation' => sqrt($variance),
+        );
     }
 }
 
