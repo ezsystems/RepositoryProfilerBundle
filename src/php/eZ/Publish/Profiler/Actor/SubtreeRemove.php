87d872592a0a805c220d02b4c811a6979b3dcc21
Kore Nordmann <mail@kore-nordmann.de>
2017-06-12 11:28:28 +0200

    Implemented executing subtree deletes through PAPI

---
+++ src/php/eZ/Publish/Profiler/Actor/SubtreeRemove.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace eZ\Publish\Profiler\Actor;
+
+use eZ\Publish\Profiler\Actor;
+use eZ\Publish\Profiler\Storage;
+
+class SubtreeRemove extends Actor
+{
+    public $storage;
+
+    public function __construct( Storage $storage )
+    {
+        $this->storage = $storage;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Remove Subtree';
+    }
+}
+
